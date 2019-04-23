358372f5a313fd8122651ebbfd4094f587acf377
Fergus <fergus.oboyle@gmail.com>
2019-04-23 12:38:31 +0100

    Working ETL pipleine tranferred to process_data.py

--- process_data.py
+++ process_data.py
@@ -1,16 +1,72 @@
 import sys
-
+import numpy as np
+import pandas as pd
+import seaborn as sns
+from sqlalchemy import create_engine
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+    """
+    Load Disaster message and Disaster message category data into a single dataframe
 
+    Args:
+        messages_filepath: filepath of the disaster message data (relative to ./data directory)
+        categories_filepath: filepath of the disaster message category data (relative to ./data directory)
+    
+    Returns:
+        A dataframe of the merged tables
+    """
+    data_directory = "./data/"
+    message_file = "messages.csv"
+    categories_file = "categories.csv"
+    database_name = "fergus2.db"
+
+    messages = pd.read_csv(data_directory + message_file)
+    categories = pd.read_csv(data_directory + categories_file)
+
+    df = messages.merge(categories, on='id', how='left')
+
+    return df
 
 def clean_data(df):
-    pass
+    """
+    Clean the disaster response messages dataframe 
 
+    Args:
+        df: input disaster response message data
+    
+    Returns:
+        Cleaned up disaster response message data
+    """
+    #first, split out the categories column into seperate columns
+    cat = df['categories'].str.split(pat=';', expand=True)
+    #grab the coloumn names from the first row (typical entry takes a formal similr to: "medical_help-0")
+    colnames = cat.iloc[1,:].str.strip('01-')
+    cat.columns = colnames
+    #remove all text except the 0 or 1 for each entry
+    cat = cat.replace(r'[a-z_]*-', '', regex=True)
+    #can now remove the original categories column
+    df = df.drop(['categories'], axis=1)
+    #add these new columns to the original dataset
+    complete_data = df.merge(cat, left_index=True, right_index=True)
+    
+    #Get rid of duplicate rows
+    complete_data.drop_duplicates(subset='id', keep='first', inplace=True)
+
+    return complete_data
 
 def save_data(df, database_filename):
-    pass  
+    """
+    Save the data to a SQLite database file 
+
+    Args:
+        df: Cleaned input disaster response message data
+        database_filename: filename to save database 
+    """
+
+    engine_name = 'sqlite:///' + database_filename
+    #using sqlalchemy
+    engine = create_engine(engine_name)
+    df.to_sql('messages_table', engine, index=False) 
 
 
 def main():
@@ -24,6 +80,7 @@ def main():
 
         print('Cleaning data...')
         df = clean_data(df)
+        print(df.shape)
         
         print('Saving data...\n    DATABASE: {}'.format(database_filepath))
         save_data(df, database_filepath)
